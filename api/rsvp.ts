9d165077ca5c5bf5f2b1e8334e45a3ad765d524b
MAC <mac@MacBook-Pro-MAC.local>
2026-07-05 20:07:32 +0400

    Fix production Telegram RSVP notification: add explicit .js extension to ESM import (ERR_MODULE_NOT_FOUND on Vercel)

--- api/rsvp.ts
+++ api/rsvp.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { parseRsvpPayload, buildRsvpMessage, sendTelegramMessage } from './_telegram'
+import { parseRsvpPayload, buildRsvpMessage, sendTelegramMessage } from './_telegram.js'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
